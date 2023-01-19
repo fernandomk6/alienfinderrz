3bffe867fbcacd70f284d808f37c9b0151b97a9d
fernando <fernandomk6@gmail.com>
2023-01-19 18:03:05 -0300

    Adicionado dependencia rickmortyapi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { getCharacters } from "rickmortyapi"
+
 const App = () => {
   // api doc
   // https://javascript.rickandmortyapi.com/index.html
+
+  // - Desenvolver a lógica de paginação
+
+  getCharacters({ name: 'rick' })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
   return (
     <div>
       <h1>Alien Finderrz</h1>
+      
       <div>
         <div>
           <form>
             <input placeholder="rick sanchez" />
           </form>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h2>Resultados</h2>
+          <ul>
+            
+          </ul>
         </div>
       </div>
     </div>
