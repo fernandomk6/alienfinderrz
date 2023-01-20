f8eb46250f30c43f33a0ac9d77a829acc505797c
fernando <fernandomk6@gmail.com>
2023-01-20 19:50:42 -0300

    Concluído exibição única de caractere

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { getCharacters } from "rickmortyapi"
+import Header from './components/Header'
 
 // api doc
 // https://javascript.rickandmortyapi.com/index.html
@@ -96,9 +97,7 @@ const App = () => {
 
   return (
     <div>
-      <header>
-        <h1>Alien Finderrz</h1>
-      </header>
+      <Header />
       
       <section>
         <div>
@@ -116,11 +115,40 @@ const App = () => {
       </section>
       
       {Boolean(caracter) && (
-        <div>
-          <span>{caracter.name}</span>
-          <button onClick={clearCaracter}>Voltar</button>
-        </div>
-
+        <section>
+          <div>
+            <img src={caracter.image} alt={caracter.name}></img>
+            <ul>
+              <li>
+                <img src='./imgs/name-icon.png' alt='' />Name: {caracter.name}
+              </li>
+              <li>
+                <img src='./imgs/gender-icon.png' alt='' />Genero: {caracter.gender}
+              </li>
+              <li>
+                <img src='./imgs/species-icon.png' alt='' />Espécie: {caracter.species}
+              </li>
+              <li>
+                <img src='./imgs/location-icon.png' alt='' />Última localização conhecida: {caracter.location.name}
+              </li>
+              <li>
+                <img src='./imgs/origin-icon.png' alt='' />Local de origem: {caracter.origin.name}
+              </li>
+              <li>
+                <img src='./imgs/status-icon.png' alt='' />Status: {caracter.status}
+              </li>
+            </ul>
+            <div>
+              <span>Episódios em que aparece</span>
+              <ul>
+                {caracter.episode.map((episode, index) => (
+                  <option key={index}>{episode}</option>
+                ))}
+              </ul>
+            </div>
+            <button onClick={clearCaracter}>Voltar</button>
+          </div>
+        </section>
       )}
  
 
