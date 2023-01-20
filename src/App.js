f3218aec5967d9170269cba940e72b3e429ef3bf
fernando <fernandomk6@gmail.com>
2023-01-20 01:00:04 -0300

    Sistema de paginação criado

--- src/App.js
+++ src/App.js
@@ -1,39 +1,124 @@
+import { useState } from 'react'
 import { getCharacters } from "rickmortyapi"
 
+// api doc
+// https://javascript.rickandmortyapi.com/index.html
+
+// - Desenvolver a lógica de paginação
+
 const App = () => {
-  // api doc
-  // https://javascript.rickandmortyapi.com/index.html
+  const [caracterName, setCaracterName] = useState('rick')
+  const [caracters, setCaracters] = useState([])
+  const [info, setInfo] = useState({})
+  const [fetching, setFetching] = useState(false)
+  const [actualPage, setActualPage] = useState(1) 
 
-  // - Desenvolver a lógica de paginação
+  const handleCaracterNameChange = (e) => {
+    setCaracterName(e.target.value)
+  }
 
-  getCharacters({ name: 'rick' })
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((error) => {
+  const handlePrevPage = async () => {
+    console.log('prev page show')
+    const prevPage = actualPage - 1
+
+
+    try {
+      const reponse = await getCharacters({ name: caracterName, page: prevPage })
+      if (reponse.status !== 200) {
+        throw new Error('Nenhum resultado encontrado')
+      }
+      
+      const caractersCopy = [...reponse.data.results]
+      const infoCopy = {...reponse.data.info}
+
+      setCaracters(caractersCopy)
+      setInfo(infoCopy)
+      setActualPage(prevPage)
+    } catch (error) {
       console.log(error)
-    })
+    }
 
+  }
+
+  const handleNextPage = async () => {
+    console.log('next page show')
+    const nextPage = actualPage + 1
+    
+    try {
+      const reponse = await getCharacters({ name: caracterName, page: nextPage })
+      if (reponse.status !== 200) {
+        throw new Error('Nenhum resultado encontrado')
+      }
+      
+      const caractersCopy = [...reponse.data.results]
+      const infoCopy = {...reponse.data.info}
+
+      setCaracters(caractersCopy)
+      setInfo(infoCopy)
+      setActualPage(nextPage)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFetching(true)
+
+    console.log(`start searching by ${caracterName}`)
+
+    try {
+      const reponse = await getCharacters({ name: caracterName })
+      if (reponse.status !== 200) {
+        throw new Error('Nenhum resultado encontrado')
+      }
+
+      const caractersCopy = [...reponse.data.results]
+      const infoCopy = {...reponse.data.info}
+
+      setCaracters(caractersCopy)
+      setInfo(infoCopy)
+    } catch (error) {
+      console.log(error)
+    }
+
+    setFetching(false)
+  }
   return (
     <div>
       <h1>Alien Finderrz</h1>
       
       <div>
         <div>
-          <form>
-            <input placeholder="rick sanchez" />
+          <form onSubmit={handleSubmit}>
+            <input 
+              value={caracterName} 
+              onChange={handleCaracterNameChange} 
+              disabled={fetching}
+              autoFocus
+            />
           </form>
         </div>
       </div>
 
-      <div>
+      {Boolean(caracters.length) && (
         <div>
-          <h2>Resultados</h2>
-          <ul>
-            
-          </ul>
+          {console.log(info)}
+          {console.log(caracters)}
+          <div>
+            <h2>{info.count} resultados</h2>
+            <ul>
+              {caracters.map((caracter) => (
+                <li key={caracter.id}>{caracter.name}</li>
+              ))}
+            </ul>
+            <span>Pagina: {actualPage} de {info.pages}</span>
+            <button onClick={handlePrevPage} disabled={!info.prev}>Página anterior</button>
+            <button onClick={handleNextPage} disabled={!info.next}>Proxíma página</button>
+          </div>
         </div>
-      </div>
+      )}
+
     </div>
   )
 }
